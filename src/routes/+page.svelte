735de95f9800ee0e876d2499afcc043de2db8714
Jesus-Delgado-24 <delgado24.dv@gmail.com>
2026-03-13 20:53:22 -0600

    Carousel
    Carousel de imagenes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
 <script>
+    import { Carousel } from "flowbite-svelte"
     import Navbar from "../lib/components/Navbar.svelte";
     import Footer from "../lib/components/Footer.svelte";
+
+
+    const imagenes = [
+        {
+            alt: "Image 1",
+            src: "/images/Img-1.png"
+        },
+        {
+            alt: "Image 2",
+            src: "/images/Img-2.png"
+        },
+        {
+            alt: "Image 3",
+            src: "/images/Img-3.png"
+        }
+    ];
 </script>
 
 <Navbar />
+
+<div class="bg-[#E9ECFF]">
+    <div class="max-w-4xl mx-auto"> 
+        <Carousel images={imagenes} duration={4000} />
+    </div>
+</div>
 
 <Footer showFooter={true} />
